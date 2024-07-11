107a1696d887a05b425f6eff0cd61338024d028d
Kev <kelvinkalumba189@gmail.com>
2024-07-11 18:39:49 +0100

    Remove API key from code and use environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,15 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBPQLHBOmXJuEiG6TbFB0gSGvjm2qv1kN8",
-  authDomain: "chat-app-67a0e.firebaseapp.com",
-  projectId: "chat-app-67a0e",
-  storageBucket: "chat-app-67a0e.appspot.com",
-  messagingSenderId: "789404745082",
-  appId: "1:789404745082:web:113e484296979f75732ac3",
-  measurementId: "G-7Q6JZ2Y99Y"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
